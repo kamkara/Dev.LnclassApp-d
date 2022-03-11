7c245b5ff953b5108ebb66505b6ae98046dc5925
kamkara <lnclass.kamkara@gmail.com>
2022-03-11 11:23:29 +0000

    add exercices styling

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,15 +1,13 @@
 class WelcomeController < ApplicationController
-  #before_action :current_user_auhenticate?
+  before_action :current_user_auhenticate?, only: %i[index]
 
   def index
-   redirect_to feeds_path if :current_user
   end
-
-  #private
-
-  #redirect current_user to feed if signed
-  # def current_user_auhenticate?
-    
-  # end
   
+  private
+  
+  #redirect current_user to feed if signed
+  def current_user_auhenticate?
+    redirect_to feeds_path if :current_user
+   end
 end
