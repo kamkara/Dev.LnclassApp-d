6036f52fa5347bbd4fb1b508714bfb9ed5f71b23
kamkara <lnclass.kamkara@gmail.com>
2022-03-19 11:19:08 +0000

    config/routes: add user routes

--- config/routes.rb
+++ config/routes.rb
@@ -32,9 +32,21 @@ Rails.application.routes.draw do
             :materials, execept: %i[new]
   
   
-  devise_for :users, path: '', path_names: { sign_in: 'Connecter', 
-              sign_out: 'logout', password: 'secret', confirmation: 'verification',
-              unlock: 'unblock', registration: '', sign_up: 'inscription-eleves' }
+  ######### USER DATA #########
+  devise_scope :user do
+    get 'profile/edit'    => 'devise/registrations#edit',   :as => :edit_user_registration
+    get 'profile/cancel'  => 'devise/registrations#cancel', :as => :cancel_user_registration
+  end
+
+  devise_for  :users,
+              :path => '',
+              :path_names => 
+              { :sign_in =>     'connexion',
+                :sign_out =>     'logout',
+                :sign_up =>       '',
+                :registration =>  'inscription',
+                :edit =>          'edit'
+              }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
